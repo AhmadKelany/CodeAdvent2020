d97283f2925330da1d267dadae911d7458d7dde2
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2020-12-07 20:31:38 +0300

    solved day 4 part 1

---
+++ CodeAdvent2020.Code/Day4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent2020.Code
+{
+    public class Day4
+    {
+        public static List<string> GetInputs() => File.ReadAllText("InputFiles/Day4.txt").Split(Environment.NewLine + Environment.NewLine).ToList();
+
+
+        public static int Part1() => GetInputs().Where(IsValid).Count();
+
+        public static bool IsValid(string passport)
+        {
+
+            var keys = passport.Split(new[]{ "\r\n" , " " },StringSplitOptions.None).Select(s => s.Substring(0, 3)).ToList();
+            var requiredKeys = new List<string>()
+            {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid"
+            };
+            var result = requiredKeys.Intersect(keys);
+            return result.Count() == 7;
+        }
+    }
+}
---
+++ CodeAdvent2020.Tests/Day4Tests.cs
@@ -0,0 +1,27 @@
+﻿using CodeAdvent2020.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeAdvent2020.Tests
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void InputsValid()
+        {
+            var inputs = Day4.GetInputs();
+            Assert.True(inputs.Any());
+        }
+        [Fact]
+        public void Part1()
+        {
+            var result = Day4.Part1();
+            Assert.True(result > 0);
+        }
+
+    }
+}
