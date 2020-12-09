761ef5f439d8594faee19cb2bf361f6ef0cffc31
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2020-12-09 16:04:28 +0300

    solved day 6 part 2

--- CodeAdvent2020.Code/Day6.cs
+++ CodeAdvent2020.Code/Day6.cs
@@ -11,7 +11,8 @@ namespace CodeAdvent2020.Code
     {
         public static List<string> GetAnswers() => File.ReadAllText("InputFiles/Day6.txt").Split(Environment.NewLine + Environment.NewLine).ToList();
         public static int Part1() => GetAnswers().Select(s => s.Replace(Environment.NewLine , "").GroupBy(c => c).Count()).Sum();
-        public static int Part2() => GetAnswers().Select(s => s.Replace(Environment.NewLine , "").GroupBy(c => c).Count()).Sum();
+        public static int Part2() => GetAnswers().
+            Select(s => s.Split(Environment.NewLine).Aggregate((x, y) => new string( x.Intersect(y).ToArray())).Length).Sum();
 
     }
 }
