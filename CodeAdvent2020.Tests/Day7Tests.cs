408f6c11c950d6a8bf63eedc0729a35cf54ebddf
Ahmad Kelany <ahmad.kelany@outlook.com>
2020-12-10 11:19:04 +0300

    started day 7

---
+++ CodeAdvent2020.Tests/Day7Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CodeAdvent2020.Code;
+
+namespace CodeAdvent2020.Tests
+{
+    public class Day7Tests
+    {
+    }
+}
