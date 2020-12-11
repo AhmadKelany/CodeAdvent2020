5877e475cf0f1351fd02c2f1b6beadfe4c164320
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2020-12-11 10:35:44 +0300

    solved day 7 part 1

--- CodeAdvent2020.Code/Day7.cs
+++ CodeAdvent2020.Code/Day7.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,33 @@ namespace CodeAdvent2020.Code
 {
     public class Day7
     {
+        public static List<string> GetRules() => File.ReadAllLines("InputFiles/Day7.txt").ToList();
+        public static List<string> GetBagColors() => GetRules().Select(s => s.Substring(0, s.IndexOf("bags")-1)).ToList();
+
+        public static List<string> GetColorsContaining(List<string> containedColors)
+        {
+            var result = GetRules().
+                Where(s =>  containedColors.Any(c => s.Contains(c) && !s.StartsWith(c))).
+                Select(s => s.Substring(0, s.IndexOf("bags") - 1)).ToList();
+
+            return result;
+        }
+
+
+        public static int Part1()
+        {
+            var containedColors = new List<string> { "shiny gold" };
+            int foundIterationCount = 0;
+            var totalContainingColors = new List<string>();
+            do
+            {
+                containedColors = GetColorsContaining(containedColors);
+                totalContainingColors.AddRange(containedColors);
+                foundIterationCount = containedColors.Count();
+            } while (foundIterationCount > 0);
+
+            return totalContainingColors.Distinct().Count();
+
+        }
     }
 }
--- CodeAdvent2020.Tests/Day7Tests.cs
+++ CodeAdvent2020.Tests/Day7Tests.cs
@@ -10,5 +10,24 @@ namespace CodeAdvent2020.Tests
 {
     public class Day7Tests
     {
+        [Fact]
+        public void InputsValid()
+        {
+            var rules = Day7.GetRules();
+            var colors = Day7.GetBagColors();
+            var containing = Day7.GetColorsContaining(new List<string> { "shiny gold" });
+            Assert.True(rules.Any());
+        }
+        [Fact]
+        public void Part1()
+        {
+            var rules = Day7.GetRules();
+            var colors = Day7.GetBagColors();
+            var containing = Day7.GetColorsContaining(new List<string> { "shiny gold" });
+
+            var result = Day7.Part1();
+            Assert.True(result > 0);
+        }
+
     }
 }
