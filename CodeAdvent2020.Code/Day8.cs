6610ac78ef6ccfd57a5b36776ee83dceed04f5d1
Ahmad Kelany <ahmad.kelany@outlook.com>
2020-12-13 13:51:21 +0300

    trying for day 8 part 2

--- CodeAdvent2020.Code/Day8.cs
+++ CodeAdvent2020.Code/Day8.cs
@@ -11,39 +11,68 @@ namespace CodeAdvent2020.Code
     {
 
         public enum Command { acc , nop , jmp}
-        public record Line(Command command , int parameter);
+        public record Line(Command Command , int Parameter , int Index);
+        private List<int> AlteredIndexes = new List<int>();
         public static List<Line> GetLines() =>
             File.ReadAllLines("InputFiles/Day8.txt").
-            Select(l => new Line(Enum.Parse<Command>(l.Substring(0, 3)), int.Parse(l.Substring(3)))).ToList();
-        public static int Part1() {
-            var lines = GetLines();
+            Select((l,i) => new Line(Enum.Parse<Command>(l.Substring(0, 3)), int.Parse(l.Substring(3)) , i)).ToList();
+        public static int Part1() => GetAccumulatorAndLastIndex(GetLines()).Accumulator;
+        public static (int Accumulator,int LastIndex , bool ExitedProperly ) GetAccumulatorAndLastIndex(List<Line> lines)
+        {
             int accumulator = 0;
             int nextIndex = 0;
-            List<int> processedLines = new List<int>();
+            bool exitedProperly = false;
+           List<int> processedLineIndexes = new List<int>();
 
-            while (!processedLines.Contains(nextIndex))
+            while (!processedLineIndexes.Contains(nextIndex))
             {
-                processedLines.Add(nextIndex);
+                if(nextIndex == lines.Count-1)
+                {
+                    exitedProperly = true;
+                    break;
+                }
+                processedLineIndexes.Add(nextIndex);
                 var line = lines[nextIndex];
-                switch (line.command)
+                switch (line.Command)
                 {
                     case Command.acc:
-                        accumulator += line.parameter;
+                        accumulator += line.Parameter;
                         nextIndex++;
                         break;
                     case Command.nop:
                         nextIndex++;
                         break;
                     case Command.jmp:
-                        nextIndex += line.parameter;
+                        nextIndex += line.Parameter;
                         break;
                     default:
                         break;
                 }
-
             }
-            return accumulator;
+           
+            return (accumulator , nextIndex , exitedProperly);
+
+
         }
-        public static int Part2() { return 0; }
+        public static int Part2() {
+            var lines = GetLines();
+            int firstIndex = -1;
+            while (true)
+            {
+                var lineToChange = lines.FirstOrDefault(l => l.Index > firstIndex && (l.Command == Command.jmp || l.Command == Command.nop));
+                firstIndex = lineToChange.Index;
+                lines[lineToChange.Index] = new Line(lineToChange.Command == Command.jmp ? Command.nop : Command.jmp, lineToChange.Parameter, lineToChange.Index);
+                var result = GetAccumulatorAndLastIndex(lines);
+                if(result.ExitedProperly)
+                {
+                    return result.Accumulator;
+                }
+                else
+                {
+                    lines[lineToChange.Index] = new Line(lineToChange.Command == Command.jmp ? Command.nop : Command.jmp, lineToChange.Parameter, lineToChange.Index);
+                }
+            }
+            
+        }
     }
 }
