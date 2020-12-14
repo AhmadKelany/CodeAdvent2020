e42b8cf675ac300e2c28c898212190c7d165e1e6
Ahmad Kelany <ahmad.kelany@outlook.com>
2020-12-14 09:04:25 +0300

    formatted day 8

--- CodeAdvent2020.Code/Day8.cs
+++ CodeAdvent2020.Code/Day8.cs
@@ -10,23 +10,24 @@ namespace CodeAdvent2020.Code
     public class Day8
     {
 
-        public enum Command { acc , nop , jmp}
-        public record Line(Command Command , int Parameter , int Index);
-        private List<int> AlteredIndexes = new List<int>();
+        public enum Command { acc, nop, jmp }
+        public record Line(Command Command, int Parameter, int Index);
         public static List<Line> GetLines() =>
             File.ReadAllLines("InputFiles/Day8.txt").
-            Select((l,i) => new Line(Enum.Parse<Command>(l.Substring(0, 3)), int.Parse(l.Substring(3)) , i)).ToList();
-        public static int Part1() => GetAccumulatorAndLastIndex(GetLines()).Accumulator;
-        public static (int Accumulator , bool ExitedProperly ) GetAccumulatorAndLastIndex(List<Line> lines)
+            Select((l, i) => new Line(Command:Enum.Parse<Command>(l.Substring(0, 3)),
+                                      Parameter: int.Parse(l.Substring(3)),
+                                      Index:i)).ToList();
+        public static int Part1() => GetAccumulatorAndExitedProperly(GetLines()).Accumulator;
+        public static (int Accumulator, bool ExitedProperly) GetAccumulatorAndExitedProperly(List<Line> lines)
         {
             int accumulator = 0;
             int nextIndex = 0;
             bool exitedProperly = false;
-           List<int> processedLineIndexes = new List<int>();
+            List<int> processedLineIndexes = new List<int>();
 
             while (!processedLineIndexes.Contains(nextIndex))
             {
-                if(nextIndex == lines.Count)
+                if (nextIndex == lines.Count)
                 {
                     exitedProperly = true;
                     break;
@@ -49,12 +50,13 @@ namespace CodeAdvent2020.Code
                         break;
                 }
             }
-           
-            return (accumulator  , exitedProperly);
+
+            return (accumulator, exitedProperly);
 
 
         }
-        public static int Part2() {
+        public static int Part2()
+        {
             var lines = GetLines();
             int firstIndex = -1;
             while (true)
@@ -63,14 +65,14 @@ namespace CodeAdvent2020.Code
                 firstIndex = lineToChange.Index;
                 lineToChange = new Line(lineToChange.Command == Command.jmp ? Command.nop : Command.jmp, lineToChange.Parameter, lineToChange.Index);
                 lines[lineToChange.Index] = lineToChange;
-                var result = GetAccumulatorAndLastIndex(lines);
-                if(result.ExitedProperly)
+                var result = GetAccumulatorAndExitedProperly(lines);
+                if (result.ExitedProperly)
                 {
                     return result.Accumulator;
                 }
                 lines[lineToChange.Index] = new Line(lineToChange.Command == Command.jmp ? Command.nop : Command.jmp, lineToChange.Parameter, lineToChange.Index);
             }
-            
+
         }
     }
 }
