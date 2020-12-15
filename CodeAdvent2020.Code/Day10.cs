383e7195a8899bf0591a3f57c649ca30ad54f35e
Ahmad Kelany <ahmad.kelany@outlook.com>
2020-12-15 12:06:15 +0300

    storm day 10

---
+++ CodeAdvent2020.Code/Day10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent2020.Code
+{
+    public class Day10
+    {
+        public static List<int> GetData() =>
+                   File.ReadAllLines("InputFiles/Day10.txt").
+                   Select(int.Parse).ToList();
+
+        public static int Part1()
+        {
+            var chargers = GetData();
+            chargers.Add(0);
+            chargers = chargers.OrderBy(c => c).ToList();
+            int onesCount = 0;
+            int threesCount = 1;
+            for (int i = 0; i < chargers.Count-1; i++)
+            {
+                if (chargers[i+1] - chargers[i] == 1)
+                {
+                    onesCount++;
+                }
+                else if (chargers[i+1] - chargers[i] == 3)
+                {
+                    threesCount++;
+                }
+
+            }
+            return onesCount * threesCount;
+        }
+
+    }
+}
---
+++ CodeAdvent2020.Tests/Day10Tests.cs
@@ -0,0 +1,21 @@
+﻿using CodeAdvent2020.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeAdvent2020.Tests
+{
+    public class Day10Tests
+    {
+        [Fact]
+        public void Part1()
+        {
+            var result = Day10.Part1();
+            Assert.True(result > 0);
+        }
+
+    }
+}
