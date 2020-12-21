96310913b54a6b820d42601e654a005aee533fcc
Ahmad Kelany <ahmad.kelany@outlook.com>
2020-12-21 16:28:38 +0300

    started day 11

---
+++ CodeAdvent2020.Code/Day11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent2020.Code
+{
+    public class Day11
+    {
+        public enum Status { Floor , Empty , Occupied}
+        public record Seat (int Id , Status Status , List<int> ConnectedSeatIds);
+        public static List<Seat> GetSeats()
+        {
+            var lines = File.ReadAllLines("InputFiles/Day11.txt");
+            var width = lines.First().Length;
+            var height = lines.Count();
+            var maxId = (width * height) - 1;
+
+
+        }
+
+    }
+}
