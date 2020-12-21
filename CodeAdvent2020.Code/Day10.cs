d71790d26c977cd935d439f4ca46e6ad80680399
Ahmad Kelany <ahmad.kelany@outlook.com>
2020-12-21 10:31:43 +0300

    done with storming day 10

--- CodeAdvent2020.Code/Day10.cs
+++ CodeAdvent2020.Code/Day10.cs
@@ -17,7 +17,7 @@ namespace CodeAdvent2020.Code
             return ImmutableList.Create(0).AddRange(chargers).Add(chargers.Last() + 3);
         }
 
-        public static int Part1OtherSolution()
+        public static int Part1_OtherSolution()
         {
             var chargers = GetData();
             var window = chargers.Skip(1).Zip(chargers).Select(p => (current: p.First, previous: p.Second));
@@ -44,9 +44,40 @@ namespace CodeAdvent2020.Code
             return onesCount * threesCount;
         }
 
+
+        public static Dictionary<int , long> MappedIndexes = new Dictionary<int, long>();
+        public static long WaysCount(ImmutableList<int> input , int index)
+        {
+            if (MappedIndexes.ContainsKey(index)) return MappedIndexes[index];
+            if (index == input.Count - 1) return 1;
+            long total = 0;
+            if (index+1 < input.Count && input[index+1] - input[index] <= 3) 
+            {
+                total += WaysCount(input, index + 1);
+            }
+            if (index+2 < input.Count && input[index+2] - input[index] <= 3) 
+            {
+                total += WaysCount(input, index + 2);
+            }
+            if (index + 3 < input.Count && input[index + 3] - input[index] <= 3)
+            {
+                total += WaysCount(input, index + 3);
+            }
+
+
+            MappedIndexes[index] = total;
+            return total;
+        }
         public static long Part2()
         {
+
             var chargers = GetData();
+
+            return WaysCount(chargers, 0);
+
+
+
+
             var (a, b, c) = (1L, 0L, 0L);
             for (var i = chargers.Count - 2; i >= 0; i--)
             {
