e83f10823541778801cc3008afb677499c4b6e9c
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-14 22:44:41 +0300

    improved the code a little

--- CodeAdvent2020.Code/2015/Day3.cs
+++ CodeAdvent2020.Code/2015/Day3.cs
@@ -11,49 +11,33 @@ namespace CodeAdvent.Code._2015
     {
         static string GetInput() => File.ReadAllText("2015/InputFiles/Day3.txt");
 
-        public static int Part1()
-        {
-            string input = GetInput();
-            var values = new HashSet<string>();
-            string currentLocation = "0,0";
-            foreach (var c in input)
-            {
-                values.Add(currentLocation);
-                currentLocation = GetNextHouseLocation(currentLocation, c);
-            }
-            var result = values.Count;
-            Screen.WriteLine($"Result = {result:N0}", ConsoleColor.Green);
-            return result;
-        }
-        public static int Part2()
-        {
-            string input = GetInput();
-            var values = new HashSet<string>();
-            string santaLocation = "0,0";
-            string roboLocation = "0,0";
-            bool isSantaTurn = false;
-            values.Add(santaLocation);
-            values.Add(roboLocation);
+        public static int Part1() => GetSeenCount(GetInput(), 1);
+        public static int Part2() => GetSeenCount(GetInput(), 2);
 
+        public static int GetSeenCount(string input , int moversCount)
+        {
+            var values = new HashSet<string>();
+            var locations = new Dictionary<int, string>();
+            string startLocation = "0,0";
+            for (int i = 0; i < moversCount; i++)
+            {
+                locations[i] = startLocation;
+            }
+            values.Add(startLocation);
+            int currentMoverIndex = 0;
             foreach (var c in input)
             {
-                if(isSantaTurn)
-                {
-                    santaLocation = GetNextHouseLocation(santaLocation, c);
-                    values.Add(santaLocation);
-                }
-                else
-                {
-                    roboLocation = GetNextHouseLocation(roboLocation, c);
-                    values.Add(roboLocation);
-                }
-                isSantaTurn = !isSantaTurn;
+                locations[currentMoverIndex] = GetNextHouseLocation(locations[currentMoverIndex], c);
+                values.Add(locations[currentMoverIndex]);
+                currentMoverIndex = currentMoverIndex == moversCount - 1 ? 0 : currentMoverIndex + 1;
             }
             var result = values.Count;
             Screen.WriteLine($"Result = {result:N0}", ConsoleColor.Green);
             return result;
 
         }
+
+
         public static string GetNextHouseLocation(string previousLocation , char direction)
         {
             int[] location = previousLocation.Split(',').Select(int.Parse).ToArray();
