ad3717cc192cbdcaebe0cfcf41dfeab5ea83ae8b
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-20 19:29:06 +0300

    added 2015 day 4

---
+++ CodeAdvent2020.Code/2015/Day4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2015
+{
+    public class Day4
+    {
+        static string input = "abcdef";
+        static int Get
+    }
+}
