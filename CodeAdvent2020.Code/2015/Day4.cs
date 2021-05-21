874a865fe8482de3eb7c8b81652086b9bddf561f
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-21 04:45:02 +0300

    approach 2

--- CodeAdvent2020.Code/2015/Day4.cs
+++ CodeAdvent2020.Code/2015/Day4.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.Collections.Concurrent;
 
 namespace CodeAdvent.Code._2015
 {
@@ -26,15 +27,38 @@ namespace CodeAdvent.Code._2015
                 }
             }
         }
+        static int Approach2(string input , int zerosCount)
+        {
+            var c = new ConcurrentQueue<int>();
+            string match = new string('0', zerosCount);
+            Parallel.ForEach(Enumerable.Range(0, int.MaxValue),
+                () => MD5.Create(),
+                (i, state, md5) =>
+                {
+                    var hashBytes = md5.ComputeHash(Encoding.ASCII.GetBytes(input + i));
+                    var hash = string.Join("", hashBytes.Select(b => b.ToString("x2")));
+
+                    if (hash.StartsWith(match))
+                    {
+                        c.Enqueue(i);
+                        state.Stop();
+                    }
+                    return md5;
+                },
+                (_) => { });
+
+            return c.Min();
+
+        }
         public static int Part1()
         {
-            int result = GetRequiredInt(input, 5);
+            int result = Approach2(input, 5);
             Screen.WriteLine($"Result = {result}", ConsoleColor.Green);
             return result;
         }
         public static int Part2()
         {
-            int result = GetRequiredInt(input, 6);
+            int result = Approach2(input, 6);
             Screen.WriteLine($"Result = {result}", ConsoleColor.Green);
             return result;
         }
