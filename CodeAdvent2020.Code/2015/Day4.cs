d0bfc66bb2dca3d4fb47b49ee42be445ce0bd121
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-21 03:55:34 +0300

    minor

--- CodeAdvent2020.Code/2015/Day4.cs
+++ CodeAdvent2020.Code/2015/Day4.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace CodeAdvent.Code._2015
 {
     public class Day4
     {
         static string input = "abcdef";
-        static int Get
+        static int GetRequiredInt(string input , int zerosCount)
+        {
+            string match = new string('0', zerosCount);
+            while (true)
+            {
+                var hex = MD5.Create();
+            }
+        }
     }
 }
