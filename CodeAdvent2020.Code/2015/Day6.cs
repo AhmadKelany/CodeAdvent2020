5684fee0e9aa8dac69fcedf98a49d7a39346aa26
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-05-27 11:37:14 +0300

    started day 6

---
+++ CodeAdvent2020.Code/2015/Day6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2015
+{
+    public class Day6
+    {
+        static List<string> GetInput() => File.ReadAllLines("2015/InputFiles/Day6.txt").ToList();
+
+    }
+}
