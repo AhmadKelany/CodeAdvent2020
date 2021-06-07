fc46eb41d68556df3127eeffc73a745a5415f742
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-06-07 08:14:59 +0300

    solved 2015 day 7

--- CodeAdvent2020.Code/2015/Day7.cs
+++ CodeAdvent2020.Code/2015/Day7.cs
@@ -10,6 +10,7 @@ namespace CodeAdvent.Code._2015
     public class Day7
     {
         public static List<string> GetInput() => File.ReadAllLines("2015/InputFiles/Day7.txt").ToList();
+        static Dictionary<string, int> wires = new Dictionary<string, int>();
         static bool IsInt(string s)
         {
             return int.TryParse(s, out _);
@@ -21,21 +22,15 @@ namespace CodeAdvent.Code._2015
             {
                 return $"C:{Command},K:{key},O1:{O1},O2:{O2},O1K:{OtherKey1},O2K:{OtherKey2}";
             }
-        }
-
-        static void AddToStack(string key , List<Action> data , Stack<Action> stack)
-        {
-            var action = data.FirstOrDefault(a => a.key == key);
-            stack.Push(action);
-            //Console.WriteLine($"added{action}");
-            
-            if (action.O1 != -1 && action.O2 != -1) return;
-            var oa1 = stack.FirstOrDefault(a => a.key == action.OtherKey1);
-            var oa2 = stack.FirstOrDefault(a => a.key == action.OtherKey2);
-            var ob1 = !stack.Any(a => a.key == action.OtherKey1);
-            var ob2 = !stack.Any(a => a.key == action.OtherKey2);
-            if (action.OtherKey1 != "" && !stack.Any(a => a.key == action.OtherKey1)) AddToStack(action.OtherKey1, data, stack);
-            if (action.OtherKey2 != "" && !stack.Any(a => a.key == action.OtherKey2)) AddToStack(action.OtherKey2, data, stack);
+            public bool CanCalculate()
+            {
+                bool result = Command switch
+                {
+                    "ASSIGN" or "NOT" => O1 > -1 || wires.ContainsKey(OtherKey1),
+                    _ => (O1 > -1 || wires.ContainsKey(OtherKey1)) && (O2 > -1 || wires.ContainsKey(OtherKey2))
+                };
+                return result;
+            }
         }
         public static Action GetAction(string s)
         {
@@ -84,14 +79,8 @@ namespace CodeAdvent.Code._2015
             }
 
         }
-        static int processed = 0;
-        public static void ApplyAction(Action action, Dictionary<string, int> wires )
+        public static void ApplyAction(Action action)
         {
-            //if ((action.O1 == -1 && !data.ContainsKey(action.OtherKey1)) || (action.O2 == -1 && !data.ContainsKey(action.OtherKey2)))
-            //{
-            //    actions.Enqueue(action);
-            //    return;
-            //}
             var o1 = action.O1 == -1 ? wires[action.OtherKey1] : action.O1;
             var o2 = action.O2 == -2 ? 0 : (action.O2 == -1 ? wires[action.OtherKey2] : action.O2);
 
@@ -101,90 +90,49 @@ namespace CodeAdvent.Code._2015
                 "AND" => o1 & o2,
                 "OR" => o1 | o2,
                 "NOT" => UInt16.MaxValue - o1,
-                "LSHIFT" => o1 >> o2,
-                "RSHIFT" => o1 << o2
+                "LSHIFT" => o1 << o2,
+                "RSHIFT" => o1 >> o2
             };
-            processed += 1;
         }
-        //public static void ProcessData(string s, Dictionary<string, int> data)
-        //{
-        //    var a = s.Split(' ');
 
-        //    switch (a.Length)
-        //    {
-        //        case 3: // assignment, to a number or to another variable
-        //            if (IsInt(a[0]) || data.ContainsKey(a[0]))
-        //            {
-        //                data[a[2]] = data.ContainsKey(a[0]) ? data[a[0]] : int.Parse(a[0]);
-        //            }
-        //            break;
-        //        case 4: // not
-
-        //            data[a[3]] = UInt16.MaxValue - int.Parse(a[1]);
-        //            break;
-        //        case 5: // and, or, lshift, rshift
-        //            int o1 = data.ContainsKey(a[0]) ? data[a[0]] : int.Parse(a[0]);
-        //            int o2 = data.ContainsKey(a[2]) ? data[a[2]] : int.Parse(a[2]);
-        //            switch (a[1])
-        //            {
-        //                case "AND":
-        //                    data[a[4]] = o1 & o2;
-        //                    break;
-        //                case "OR":
-        //                    data[a[4]] = o1 | o2;
-        //                    break;
-        //                case "LSHIFT":
-        //                    data[a[4]] = o1 >> o2;
-        //                    break;
-        //                case "RSHIFT":
-        //                    data[a[4]] = o1 << o2;
-        //                    break;
-        //                default:
-        //                    break;
-        //            }
-        //            break;
-        //        default:
-        //            break;
-        //    }
-        //}
-
-
-
-        public static int Part1()
+        public static int Part2()
         {
-            var stack = new Stack<Action>();
+            var aSignalFromPart1 = Part1();
             var allActions = GetInput().Select(GetAction).ToList();
-            AddToStack("a", allActions, stack);
-            var t = stack.Count;
+            var bAction = allActions.FirstOrDefault(a => a.key == "b");
+            allActions.Remove(bAction);
+            bAction = new Action("ASSIGN", "", "", aSignalFromPart1, -2, "b");
+            allActions.Add(bAction);
+            var result = ProcessActions(allActions);
+            Screen.WriteLine($"Part 2 result = {result}" , ConsoleColor.Cyan);
+            return result;
 
+        }
 
-
-
-
-
-
-            var wires = new Dictionary<string, int>();
-            //var actions = new Queue<Action>();
-            //GetInput().Reverse();
-            //foreach (var input in GetInput())
-            //{
-            //    actions.Enqueue(GetAction(input));
-            //}
-            //var stack = new Stack<Action>();
-            //while (true)
-            //{
-
-            //}
-            //var u = actions.Select(a => a.key).Distinct();
-            while (stack.Count > 0)
+        private static int ProcessActions(List<Action> allActions)
+        {
+            wires.Clear();
+            while (allActions.Count > 0)
             {
-                var a = stack.Pop();
-                ApplyAction(a, wires);
+                var action = allActions.FirstOrDefault(a => a.CanCalculate());
+                ApplyAction(action);
+                allActions.Remove(action);
+
             }
+
+
             var result = wires["a"];
 
 
-            Screen.WriteLine($"Part 1 result = {result}");
+            return result;
+        }
+
+        public static int Part1()
+        {
+            
+            var allActions = GetInput().Select(GetAction).ToList();
+            var result = ProcessActions(allActions);
+            Screen.WriteLine($"Part 1 result = {result}" , ConsoleColor.Green);
             return result;
            
         }
