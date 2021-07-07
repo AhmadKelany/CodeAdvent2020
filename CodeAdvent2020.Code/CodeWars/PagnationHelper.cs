2f3102b34aac59e6e2e5c6742722d7cb64107dac
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-07-07 11:06:35 +0300

    solved codewars pagination helper

---
+++ CodeAdvent2020.Code/CodeWars/PagnationHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code.CodeWars
+{
+    public class PagnationHelper<T>
+    {
+        private IList<T> _collection = new List<T>();
+        private int _itemsPerPage;
+        // TODO: Complete this class
+
+        /// <summary>
+        /// Constructor, takes in a list of items and the number of items that fit within a single page
+        /// </summary>
+        /// <param name="collection">A list of items</param>
+        /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+        public PagnationHelper(IList<T> collection, int itemsPerPage)
+        {
+            _collection = collection;
+            _itemsPerPage = itemsPerPage;
+        }
+
+        /// <summary>
+        /// The number of items within the collection
+        /// </summary>
+        public int ItemCount => _collection.Count;
+
+        /// <summary>
+        /// The number of pages
+        /// </summary>
+        public int PageCount => ItemCount / _itemsPerPage + (ItemCount % _itemsPerPage == 0 ? 0 : 1);
+
+        /// <summary>
+        /// Returns the number of items in the page at the given page index 
+        /// </summary>
+        /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+        /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+        public int PageItemCount(int pageIndex)
+        {
+            if (pageIndex < 0 || pageIndex >= PageCount) return -1;
+            return pageIndex == PageCount - 1 ? ItemCount % _itemsPerPage : _itemsPerPage;
+        }
+
+        /// <summary>
+        /// Returns the page index of the page containing the item at the given item index.
+        /// </summary>
+        /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+        /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+        public int PageIndex(int itemIndex)
+        {
+            if(itemIndex < 0 || itemIndex >= ItemCount) return -1;
+            return itemIndex / _itemsPerPage;
+        }
+
+        
+    }
+}
