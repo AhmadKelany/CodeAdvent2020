c92bae2efe66b506443d03b4fbd74b1bad893772
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-07-16 18:48:34 +0300

    started a codewars challnge

---
+++ CodeAdvent2020.Code/CodeWars/Logic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code.CodeWars
+{
+    public class Logic
+    {
+        public static string Extract(int[] args)
+        {
+        }
+    }
+}
