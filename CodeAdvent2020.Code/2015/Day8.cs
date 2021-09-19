abd7793b56671a1648a7d32b57ffafe9de29e031
Ahmad Kelany Laptop <ahmad.kelany@outlook.com>
2021-09-19 09:54:43 +0300

    solved 2015 day 8 part 1 with sample data, actual data fails

--- CodeAdvent2020.Code/2015/Day8.cs
+++ CodeAdvent2020.Code/2015/Day8.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeAdvent.Code._2015
 {
     public class Day8
     {
+        public static List<string> GetInput() => File.ReadAllLines("2015/InputFiles/Day8.txt").Select(s=>s.Trim('"')).ToList();
+        public static (int codeLength,int memoryLength) GetStringLengths(string s)
+        {
+            s = s.Replace("\n", "");
+            int codeLength = s.Length + 2;
+            s = s.Replace("\\\"" , "i" );
+            s = Regex.Replace(s, @"\\x[0-9]{2}", "i");
+            s = s.Replace("\\", "i");
+            int memoryLength = s.Length;
+            return (codeLength,memoryLength);
+        }
+        public static void Part1()
+        {
+            var result = GetInput()
+                .Select(GetStringLengths)
+                .Sum(x => x.codeLength - x.memoryLength);
+            Screen.WriteLine($"Part 1 result = {result}" , ConsoleColor.Cyan);
+        }
     }
+
 }
---
+++ CodeAdvent2020.Tests/2015Tests/Day8Tests.cs
@@ -0,0 +1,26 @@
+﻿using CodeAdvent.Code._2015;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeAdvent.Tests._2015Tests
+{
+    public class Day8Tests
+    {
+        [Theory]
+        [InlineData("", 2, 0)]
+        [InlineData("abc", 5, 3)]
+        [InlineData("aaa\"aaa", 10, 7)]
+        [InlineData("\x27", 6, 1)]
+        public void GetStringLengthsTest(string s, int expectedCodeLength, int expectedMemoryLength)
+        {
+            var lengths = Day8.GetStringLengths(s);
+            Assert.Equal(expectedMemoryLength, lengths.memoryLength);
+
+            Assert.Equal(expectedCodeLength , lengths.codeLength);
+        }
+    }
+}
