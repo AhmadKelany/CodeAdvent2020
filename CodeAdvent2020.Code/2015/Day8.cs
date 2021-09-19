f55331f0a8eed28eec601d40b5723f098dc49e82
Ahmad Kelany Laptop <ahmad.kelany@outlook.com>
2021-09-19 15:36:35 +0300

    solved part 1

--- CodeAdvent2020.Code/2015/Day8.cs
+++ CodeAdvent2020.Code/2015/Day8.cs
@@ -10,28 +10,18 @@ namespace CodeAdvent.Code._2015
 {
     public class Day8
     {
-        public static List<string> GetInput() => File.ReadAllLines("2015/InputFiles/Day8.txt").Select(s=>s.Trim('"')).ToList();
-        public static (int codeLength,int memoryLength) GetStringLengths(string s)
-        {
-            s = s.Replace("\n", "");
-            int codeLength = s.Length + 2;
-            s = s.Replace("\\\"" , "i" );
-            s = Regex.Replace(s, @"\\x[0-9]{2}", "i");
-            s = s.Replace("\\", "i");
-            int memoryLength = s.Length;
-            return (codeLength,memoryLength);
-        }
+        public static List<string> GetInput() => File.ReadAllLines("2015/InputFiles/Day8.txt").ToList();
         public static string GetInMemoryString(string s)
         {
-            s = s.Replace("\\\"", "i");
-            s = Regex.Replace(s, @"\\x[0-9]{2}", "i");
-            s = s.Replace("\\\\", "i");
+            s = s.Substring(1,s.Length-2).Replace("\\\"", "\"");
+            s = s.Replace("\\\\", "?");
+            s = Regex.Replace(s, @"\\x[0-9a-f]{2}", "?");
             return s;
         }
         public static void Part1()
         {
             var result = GetInput()
-                .Sum(x => x.Length + 2  - GetInMemoryString(x).Length);
+                .Sum(x => x.Length  - GetInMemoryString(x).Length);
             Screen.WriteLine($"Part 1 result = {result}" , ConsoleColor.Cyan);
         }
     }
