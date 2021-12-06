7e907ac43383cc7371c63934a312cc613feece0d
Ahmad Kelany <ahmad.kelany@outlook.com>
2021-12-06 20:04:37 +0300

    started day 3

---
+++ CodeAdvent2020.Code/2021/Day3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2021
+{
+    public static class Day3
+    {
+        public static List<string> GetData() =>
+    File.ReadAllLines("2021/InputFiles/Day3.txt").ToList();
+
+        public static int Part1()
+        {
+
+        }
+    }
+}
