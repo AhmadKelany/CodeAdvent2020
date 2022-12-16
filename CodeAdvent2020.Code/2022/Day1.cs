cbacc029000316e2d59b1a4bd95259a86bdaf45c
Ahmad Kelany <ahmad.kelany@yahoo.com>
2022-12-16 23:14:01 +0300

    finished 2022 day 1 part 1

--- CodeAdvent2020.Code/2022/Day1.cs
+++ CodeAdvent2020.Code/2022/Day1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,4 +9,34 @@ namespace CodeAdvent.Code._2022;
 
 public class Day1
 {
+    public static string[] GetData() =>
+    File.ReadAllLines(@"2022/InputFiles/Day1.txt");
+    public static string[] GetSampleData() =>
+    File.ReadAllLines(@"2022/InputFiles/Day1Sample.txt");
+
+    public static int Part1()
+    {
+        var d = GetData().AsSpan<string>();
+        int max = 0;
+        int elfTotal = 0;
+        int i = 0;
+        while (i < d.Length)
+        {
+            int.TryParse(d[i], out int item);
+            elfTotal += item;
+            if (d[i] == "" || i == d.Length -1)
+            {
+                max = Math.Max(max, elfTotal);
+                elfTotal = 0;
+            }
+            i++;
+        }
+        Screen.WriteLine($"result = {max}", ConsoleColor.Green);
+        return max;
+    }
+
+    public static int Part2()
+    {
+        return 0;
+    }
 }
---
+++ CodeAdvent2020.Tests/2022Tests/Day1Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeAdvent.Tests._2022Tests;
+
+public class Day1Tests
+{
+    [Fact]
+    public void Part1ShouldWork()
+    {
+        int i = CodeAdvent.Code._2022.Day1.Part1();
+        Assert.Equal(0,i);  
+    }
+}
