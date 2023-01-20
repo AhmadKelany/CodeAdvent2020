d559e23d021318aabc3e7968277ab14088e8978d
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-01-20 15:33:48 +0300

    formatting

--- CodeAdvent2020.Code/Other/CrackCode.cs
+++ CodeAdvent2020.Code/Other/CrackCode.cs
@@ -8,71 +8,84 @@ namespace CodeAdvent.Code.Other;
 
 public class CrackCode
 {
-    
+
     public record Clue(int[] Numbers, int CorrectNumberCount, int CorrectPlaceCount);
 
     public static string Solve(List<Clue> clues)
     {
+        /* In this dictionary, the key is the digit of the final solution, the value is the index of that digit in the final solution
+         * e.g. if the final solution is: 042, we want the dictionary to be {0(this is the digit) : 0(this is the index of the digit in the solution) , 4:1 , 2:2} */
         Dictionary<int, int> result = new();
+
+
         Clue allWrongClue = clues.First(c => c.CorrectNumberCount == 0 && c.CorrectPlaceCount == 0);
         HashSet<int> wrongNumbers = allWrongClue.Numbers.ToHashSet();
         clues.Remove(allWrongClue);
+
+
         List<int> positions = new() { 0, 1, 2 };
         List<NumberPositions> numberPositionsInfos = new();
         List<int> uniqueNumbers = clues.SelectMany(c => c.Numbers).Where(n => !wrongNumbers.Contains(n)).Distinct().ToList();
+
+
         foreach (int number in uniqueNumbers)
         {
-            NumberPositions np = new NumberPositions { Number = number, RightPosition = -1 };
-            numberPositionsInfos.Add(np);
+            NumberPositions numberPosistions = new NumberPositions { Number = number, RightPosition = -1 }; // -1 index indicate that the index is unknow
+            numberPositionsInfos.Add(numberPosistions);
             foreach (var clue in clues)
             {
-
-                if (clue.Numbers.Contains(number))
+                if (!clue.Numbers.Contains(number)) continue;
+                int index = Array.IndexOf(clue.Numbers, number);
+                switch (clue.CorrectPlaceCount)
                 {
-                    int index = Array.IndexOf(clue.Numbers, number);
-                    if (clue.CorrectPlaceCount == 1)
-                    {
-                        np.RightPosition = index;
-                    }
-                    else
-                    {
-                        np.WrongPositions.Add(index);
-                    }
+                    case 1:
+                        numberPosistions.RightPosition = index;
+                        break;
+                    default:
+                        numberPosistions.WrongPositions.Add(index);
+                        break;
                 }
+
             }
 
 
         }
-               
+
         numberPositionsInfos = numberPositionsInfos.
-            Where(n =>   !n.WrongPositions.Contains(n.RightPosition) &&
-                                              !(n.WrongPositions.Count == 0)).
+            Where(n => !n.WrongPositions.Contains(n.RightPosition) &&
+                                            !(n.WrongPositions.Count == 0)).
             ToList();
 
-        var firstNumber = numberPositionsInfos.First(n =>  n.RightPosition != -1 && !wrongNumbers.Contains(n.Number));
-        result[firstNumber.Number] = firstNumber.RightPosition;
-        var secondNumber = numberPositionsInfos.First(n => n.WrongPositions.Count == 2);
-        result[secondNumber.Number] = positions.Except(secondNumber.WrongPositions).Single();
-        foreach (var c in clues.
+        NumberPositions firstNumberPositionInfo = numberPositionsInfos.First(n => n.RightPosition != -1 && !wrongNumbers.Contains(n.Number));
+        result[firstNumberPositionInfo.Number] = firstNumberPositionInfo.RightPosition;
+
+
+        NumberPositions secondNumberPositionInfo = numberPositionsInfos.First(n => n.WrongPositions.Count == 2);
+        result[secondNumberPositionInfo.Number] = positions.Except(secondNumberPositionInfo.WrongPositions).Single();
+
+        IEnumerable<Clue> cluesWithOneCorrectDigitThatIsOneOfFoundDigits = clues.
             Where(c => c.CorrectNumberCount == 1 &&
-                               c.CorrectPlaceCount == 0 && 
-                               (c.Numbers.Contains(firstNumber.Number) || c.Numbers.Contains(secondNumber.Number))))
+            c.CorrectPlaceCount == 0 &&
+            (c.Numbers.Contains(firstNumberPositionInfo.Number) || c.Numbers.Contains(secondNumberPositionInfo.Number)));
+
+        foreach (var c in cluesWithOneCorrectDigitThatIsOneOfFoundDigits) 
         {
-            var ns = c.Numbers.Where(n => n != firstNumber.Number && n != secondNumber.Number);
-            foreach (var w in ns)
+            // in a clue that has one correct digit and has one of the two solution digits we discoverd, the remaining digits in that clue are obviously wrong
+            IEnumerable<int> digitsOtherThanTheCorrectDigits = c.Numbers.Where(n => n != firstNumberPositionInfo.Number && n != secondNumberPositionInfo.Number);
+            foreach (int d in digitsOtherThanTheCorrectDigits)
             {
-                wrongNumbers.Add(w);
+                wrongNumbers.Add(d);
             }
         }
-        positions.Remove(result[firstNumber.Number]);
-        positions.Remove(result[secondNumber.Number]);
-        numberPositionsInfos.Remove(firstNumber);
-        numberPositionsInfos.Remove(secondNumber);
-        var thirdNumber = numberPositionsInfos.
+        positions.Remove(result[firstNumberPositionInfo.Number]);
+        positions.Remove(result[secondNumberPositionInfo.Number]);
+        numberPositionsInfos.Remove(firstNumberPositionInfo);
+        numberPositionsInfos.Remove(secondNumberPositionInfo);
+        NumberPositions thirdNumberPositionInfo = numberPositionsInfos.
             Where(n => !wrongNumbers.Contains(n.Number) &&
-                                            n.WrongPositions.Intersect(positions).Count() == 0).
-                                            First();
-        result[thirdNumber.Number] = positions[0];
-        return string.Concat( result.OrderBy(d => d.Value).ToDictionary(d => d.Key, d => d.Value).Select(d => d.Key)) ;
+            n.WrongPositions.Intersect(positions).Count() == 0).
+            First();
+        result[thirdNumberPositionInfo.Number] = positions[0];
+        return string.Concat(result.OrderBy(d => d.Value).ToDictionary(d => d.Key, d => d.Value).Select(d => d.Key));
     }
 }
