516cd9afc6ea23d9fa9ce509865ac33a054043a5
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-12-03 10:36:55 +0300

    finished 2023 day 2

---
+++ CodeAdvent2020.Code/2023/Day2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2023;
+
+public class Day2
+{
+    static List<string> GetInput() => File.ReadAllLines("2023/InputFiles/Day2.txt").ToList();
+    public static Cubes AvailabeCubesForPart1 => new Cubes(12, 13, 14);
+
+    public record Cubes(int Red, int Green, int Blue);
+    public class Game
+    {
+        public int Id { get; set; }
+        public List<Cubes> Cubes { get; set; }
+    }
+
+    public static bool IsValidGame(Game game, Cubes availableCubes)
+    {
+        bool isInvalid = game.
+            Cubes.
+            Any(d => d.Red > availableCubes.Red ||
+                             d.Green > availableCubes.Green ||
+                             d.Blue > availableCubes.Blue);
+
+        return !isInvalid;
+    }
+
+
+    public static Cubes GetMinimumRequiredCubes(IEnumerable<Cubes> draws)
+    {
+        return new Cubes(draws.Max(d => d.Red),
+            draws.Max(d => d.Green),
+            draws.Max(d => d.Blue));
+    }
+
+
+    public static int GetPowerOfCubes(Cubes cubes)
+    {
+        return cubes.Red * cubes.Green * cubes.Blue;
+    }
+
+    public static Game GetGame(string gameLine)
+    {
+        var parts = gameLine.Split(':');
+        int gameId = int.Parse(parts[0].Substring(5));
+        List<Cubes> cubes = parts[1].Split(';').Select(GetCubes).ToList();
+        var game = new Game()
+        {
+            Id = gameId,
+            Cubes = cubes,
+        };
+        return game;
+    }
+    public static Cubes GetCubes(string cubesText)
+    {
+        var parts = cubesText.Trim().Split(',');
+        int red = 0, green = 0, blue = 0;
+        foreach (string part in parts)
+        {
+            var valueAndColor = part.Trim().Split(' ');
+            int value = int.Parse(valueAndColor[0]);
+            string color = valueAndColor[1];
+            switch(color)
+            {
+                case "red":
+                    red = value;
+                    break;
+                case "green":
+                    green = value;
+                    break;
+                case "blue":
+                    blue = value;
+                    break;
+                    default:
+                    throw new NotSupportedException("The text value is not supported.");
+            }
+        }
+        return new(Red : red, Blue: blue, Green: green);
+    }
+
+    public static void Part1()
+    {
+
+        var validGames = GetInput().
+            Select(GetGame).
+            Where(g => IsValidGame(g, AvailabeCubesForPart1));
+
+        int result = validGames.Sum(g => g.Id);
+        Screen.WriteLine($"Part 1 Result = {result}", ConsoleColor.Green);
+
+    }
+
+    public static void Part2()
+    {
+        int result = GetInput().
+            Select(GetGame).
+            Select(g => GetMinimumRequiredCubes(g.Cubes)).
+            Select(GetPowerOfCubes).
+            Sum();
+
+        Screen.WriteLine($"Part 2 Result = {result}", ConsoleColor.Green);
+
+    }
+}
---
+++ CodeAdvent2020.Tests/2023Tests/Day2Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeAdvent.Code._2023;
+
+using Xunit;
+using static CodeAdvent.Code._2023.Day2;
+
+namespace CodeAdvent.Tests._2023Tests;
+
+public class Day2Tests
+{
+    [Theory]
+    [InlineData("3 green, 6 red" , 6,3,0)]
+    [InlineData("3 green, 4 blue, 1 red", 1 , 3 , 4)]
+    public void GetDrawTest(string drawText , int red , int green , int blue)
+    {
+        Assert.Equal(new Cubes(red, green, blue), GetCubes(drawText));
+    }
+
+    [Fact]
+    public void GetGameTest()
+    {
+        string line = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red";
+        Game game = GetGame(line);
+        Assert.Equal(4, game.Id);
+        Assert.Equal(3, game.Cubes.Count);
+        Assert.Equal(new(3 , 1, 6), game.Cubes[0]);
+        Assert.Equal(new(6 , 3, 0), game.Cubes[1]);
+        Assert.Equal(new(14 , 3 , 15), game.Cubes[2]);
+    }
+   
+    [Theory]
+    [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true)]
+    [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true)]
+    [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false)]
+    [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red" , false)]
+    [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", true)]
+    public void IsValidGameTest(string gameLine, bool isValidExpected)
+    {
+        Game game = GetGame(gameLine);
+        bool isValidActual = IsValidGame(game , Day2.AvailabeCubesForPart1);
+        Assert.Equal(isValidExpected, isValidActual);
+    }
+
+    [Fact]
+    public void GetPowerOfDrawTest()
+    {
+        Cubes draw = new(14, 5, 3);
+        Assert.Equal(210, GetPowerOfCubes(draw));
+    }
+
+    [Theory]
+    [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", 48)]
+    [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", 12)]
+    [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", 1560)]
+    [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", 630)]
+    [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", 36)]
+    public void GetMinimumRequiredCubesPowerTest(string gameLine , int expectedPower)
+    {
+
+        Game game = GetGame(gameLine);
+        Cubes minimumRequiredCubes = GetMinimumRequiredCubes(game.Cubes);
+        int actualPower = GetPowerOfCubes(minimumRequiredCubes);
+        Assert.Equal(expectedPower, actualPower);
+    }
+}
