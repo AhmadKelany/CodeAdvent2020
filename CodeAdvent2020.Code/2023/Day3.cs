deda75b864e1f58763c0c204461da5d909059528
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-12-06 23:27:46 +0300

    trying on day 3

---
+++ CodeAdvent2020.Code/2023/Day3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2023;
+
+public class Day3
+{
+    const string Symbols = @"!@#$%^&*+-*/\";
+    public record Point(int X , int Y);
+    public record PartNumber(int Number, List<Point> Points):EqualityComparer<PartNumber>
+    {
+
+        public override bool Equals(PartNumber x, PartNumber y)
+        {
+            return x.Number == y.Number && x.Points.All(p => y.Points.Contains(p)) && y.Points.All(p => x.Points.Contains(p));
+        }
+
+        public override int GetHashCode([DisallowNull] PartNumber obj)
+        {
+            throw new NotImplementedException();
+        }
+    };
+    static List<string> GetInput() => File.ReadAllLines("2023/InputFiles/Day3.txt").ToList();
+    public static List<PartNumber> GetPartNumbers(List<string> lines)
+    {
+        var parts = new List<PartNumber>();
+        for (int x = 0; x < lines.Count; x++)
+        {
+            char[] allSeparators = ['.', .. Symbols.ToArray()];
+            var numbers = lines[x].Split( allSeparators , StringSplitOptions.RemoveEmptyEntries);
+            foreach (var number in numbers)
+            {
+                int y = lines[x].IndexOf(number);
+                List<Point> points = new List<Point>();
+                for (int z = 0; z <= numbers.Length; z++)
+                {
+                    points.Add(new(x, y + z));
+                }
+                parts.Add(new(int.Parse(number), points));
+            }
+        }
+        return parts;
+    }
+}
---
+++ CodeAdvent2020.Tests/2023Tests/Day3Tests.cs
@@ -0,0 +1,33 @@
+﻿using CodeAdvent.Code._2023;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CodeAdvent.Tests._2023Tests;
+
+public class Day3Tests
+{
+    static List<string> lines = ["467..114..", "...*......", "..35..633.", "......#...", "617*......", ".....+.58.", "..592.....", "......755.", "...$.*....", ".664.598.."];
+    [Fact]
+    public void GetPartNumbersTest()
+    {
+        var parts = Day3.GetPartNumbers(lines);
+        Assert.Equal(10, parts.Count);
+        Assert.Equal( new(467 , [new (0,0),new (0,1),new (0,2)]) , parts[0]);
+        
+        Assert.Equal( new(114 , [new (0,5),new (0,6),new (0,7)]) , parts[1]);
+        Assert.Equal( new(35 , [new (2,2),new (2,3)]) , parts[2]);
+        Assert.Equal( new(633 , [new (2,6),new (2,7),new (2,8)]) , parts[3]);
+        Assert.Equal( new(617 , [new (4,0),new (4,1),new (4,2)]) , parts[4]);
+        Assert.Equal( new(58 , [new (5,7),new (5,8)]) , parts[5]);
+        Assert.Equal( new(592 , [new (6,2),new (6,3),new (6,4)]) , parts[6]);
+        Assert.Equal( new(755 , [new (7,6),new (7,7),new (7,8)]) , parts[7]);
+        Assert.Equal( new(664 , [new (9,1),new (9,2),new (9,3)]) , parts[8]);
+        Assert.Equal( new(598 , [new (9,5),new (9,6),new (9,7)]) , parts[9]);
+    }
+}
