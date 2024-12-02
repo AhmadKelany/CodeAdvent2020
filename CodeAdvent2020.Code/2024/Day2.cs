3aa186bbb990e39ba5af457d789116d4ebdf8578
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2024-12-03 01:13:31 +0300

    started day 2

--- CodeAdvent2020.Code/2024/Day2.cs
+++ CodeAdvent2020.Code/2024/Day2.cs
@@ -18,25 +18,36 @@ public static class Day2
         return splits.Select(int.Parse).ToArray();
     }
 
-    static bool IsSafe(int[] levels)
+    static bool IsSafe(int[] levels, int tolerateCount = 0)
     {
         if (levels[0] == levels[1]) return false;
         int factor = levels[1] > levels[0] ? 1 : -1;
         int index = 1;
         while (index < levels.Length) 
         { 
-            if(levels[index] == levels[index - 1] || factor * (levels[index] - levels[index-1]) > 3) return false;
+            int diff = levels[index] - levels[index - 1];
+            if (diff == 0 || diff / Math.Abs(diff) != factor || Math.Abs( diff) > 3) tolerateCount--;
+            if(tolerateCount < 0) return false;
             index++;
         }
         return true;
     }
     public static void Part1()
     {
-        var lines = GetSampleInput();
+        var lines = GetInput();
         var levels = lines.
             Select(GetLevels);
         int result = levels.
-            Count(IsSafe);
+            Count(l => IsSafe(l));
         Screen.WriteLine($"Part 1 Result = {result}", ConsoleColor.Cyan);
+    }
+    public static void Part2()
+    {
+        var lines = GetInput();
+        var levels = lines.
+            Select(GetLevels);
+        int result = levels.
+            Count(l => IsSafe(l,1));
+        Screen.WriteLine($"Part 2 Result = {result}", ConsoleColor.Cyan);
     }
 }
