ab03695632494a0ba952c141dd8e5f0ad9b32521
Ahmad Kelany <ahmad.kelany@yahoo.com>
2024-12-02 08:06:08 +0300

    finished 2024 day1

--- CodeAdvent2020.Code/2024/Day1.cs
+++ CodeAdvent2020.Code/2024/Day1.cs
@@ -9,7 +9,49 @@ namespace CodeAdvent.Code._2024;
 
 public static class Day1
 {
-    static string[] GetInput() => File.ReadAllLines("2023/InputFiles/Day4.txt");
-    static string[] GetSampleInput() => File.ReadAllLines("2023/InputFiles/Day4Sample.txt");
+    static string[] GetInput() => File.ReadAllLines("2024/InputFiles/Day1.txt");
+    static string[] GetSampleInput() => File.ReadAllLines("2024/InputFiles/Day1Sample.txt");
+    static (int Num1, int Num2) GetNumbers(string line)
+    {
+        var splits = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return (int.Parse(splits[0]), int.Parse(splits[1]));
+    }
 
+    public static void Part1()
+    {
+        var lines = GetInput();
+        var numbers = lines.
+            Select(GetNumbers);
+        List<int> list1 = new();
+        List<int> list2 = new();
+        foreach(var (num1, num2) in numbers)
+        {
+            list1.Add(num1);
+            list2.Add(num2);
+        }
+        list1.Sort();
+        list2.Sort();
+        int result = list1.Zip(list2, (num1, num2) => Math.Abs( num1 - num2)).Sum();
+        Screen.WriteLine($"Part 1 Result = {result}", ConsoleColor.Cyan);
+    }
+
+    public static void Part2()
+    {
+        var lines = GetInput();
+        var numbers = lines.
+            Select(GetNumbers);
+        List<int> list1 = new();
+        List<int> list2 = new();
+        foreach (var (num1, num2) in numbers)
+        {
+            list1.Add(num1);
+            list2.Add(num2);
+        }
+        Dictionary<int, int> dict = new();
+        dict = list2.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+        int result = list1.
+            Select(n => dict.ContainsKey(n)? n * dict[n] : 0).
+            Sum();
+        Screen.WriteLine($"Part 2 Result = {result}", ConsoleColor.Cyan);
+    }
 }
