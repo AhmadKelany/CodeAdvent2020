f484b2cdc218689f31f21ae1ab277225d3eec282
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2024-12-05 20:29:06 +0300

    Add file reading methods and initial matrix processing
    Added `using System.IO;` for file operations. Introduced `GetInput` and `GetSampleInput` methods to read lines from specific files. Added `GetMatrix` to convert string arrays to jagged char arrays. Implemented `GetPossibleCount` with partial functionality.

--- CodeAdvent2020.Code/2024/Day4.cs
+++ CodeAdvent2020.Code/2024/Day4.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,4 +9,30 @@ namespace CodeAdvent.Code._2024;
 
 public static class Day4
 {
+    static string[] GetInput() => File.ReadAllLines("2024/InputFiles/Day4.txt");
+    static string[] GetSampleInput() => File.ReadAllLines("2024/InputFiles/Day4Sample.txt");
+    static char[][] GetMatrix(string[] input)
+    {
+        char[][] matrix = new char[input.Length][];
+        for (int i = 0; i < input.Length; i++)
+        {
+            matrix[i] = input[i].ToArray();
+        }
+        return matrix;
+    }
+    static void GetPossibleCount(int[][] matrix, int x,int y)
+    {
+        int count = 0;
+        int startX = x <= 3 ? 0 : x - 3;
+        int endX = x >= matrix[0].Length - 3 ? matrix[0].Length : x + 3;
+        int startY = y <= 3 ? 0 : y - 3;
+        int endY = y >= matrix.Length - 3 ? matrix.Length : y + 3;
+        for (int c = startX; c <= endX; c++)
+        {
+            for(int r = startY; r <= endY; r++)
+            {
+                
+            }
+        }
+    }
 }
