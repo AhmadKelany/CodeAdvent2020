e64f233dc2fe1c473a30c6ec5125eac31b3f2109
Ahmad Kelany <ahmad.kelany@yahoo.com>
2024-12-05 10:57:50 +0300

    Add Day4.cs and update Day4Sample.txt with new data
    Introduced a new C# source file named `Day4.cs` within the `CodeAdvent.Code._2024` namespace. The file includes several `using` directives for common .NET namespaces and defines a new static class `Day4` without any methods or properties.
    
    Added 10 new lines of text to `Day4Sample.txt`, containing sequences of characters 'M', 'S', 'A', and 'X'.

---
+++ CodeAdvent2020.Code/2024/Day4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2024;
+
+public static class Day4
+{
+}
