96966414a88a6b31d2b22e75fe4e8a39118f3c27
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2024-12-10 22:18:46 +0300

    Add Day5 class with methods to read input files
    Added necessary using directives for various namespaces.
    Introduced new namespace CodeAdvent.Code._2024.
    Created public class Day5 with static methods GetInput and GetSampleInput.
    Methods read lines from Day5.txt and Day5Sample.txt in 2024/InputFiles.

---
+++ CodeAdvent2020.Code/2024/Day5.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAdvent.Code._2024;
+
+public class Day5
+{
+    static string[] GetInput() => File.ReadAllLines("2024/InputFiles/Day5.txt");
+    static string[] GetSampleInput() => File.ReadAllLines("2024/InputFiles/Day5Sample.txt");
+    
+}
