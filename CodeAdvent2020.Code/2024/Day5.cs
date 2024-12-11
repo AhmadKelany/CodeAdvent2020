41277cc85451422ea016369cde200a8d43742431
Ahmad Kelany <ahmad.kelany@yahoo.com>
2024-12-11 14:58:11 +0300

    m

--- CodeAdvent2020.Code/2024/Day5.cs
+++ CodeAdvent2020.Code/2024/Day5.cs
@@ -9,7 +9,14 @@ namespace CodeAdvent.Code._2024;
 
 public class Day5
 {
-    static string[] GetInput() => File.ReadAllLines("2024/InputFiles/Day5.txt");
-    static string[] GetSampleInput() => File.ReadAllLines("2024/InputFiles/Day5Sample.txt");
+    static string GetInput() => File.ReadAllText("2024/InputFiles/Day5.txt");
+    static string GetSampleInput() => File.ReadAllText("2024/InputFiles/Day5Sample.txt");
+
+    record Order(int First,int Second);
+    static Dictionary<int, int[]> GetOrders(string input)
+    {
+        string[] s = input.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<int, int[]> orders = new();
+    }
     
 }
